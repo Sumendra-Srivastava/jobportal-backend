07de10f326e214cb4613fef7625065f999847aad
Sumendra-Srivastava <somusrivastav871@gmail.com>
2024-11-15 22:19:51 +0530

    Change cors options

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-frontend-72nn.onrender.com',
     credentials:true
 }
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
+``
 
 // api's
 app.use("/api/v1/user", userRoute);
